4c09bfdbe9a5e47fb820a22cc4f38545e3d12c26
Devang Gaur <devang.only@gmail.com>
2017-04-16 00:26:28 +0530

    gulpfile edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,3 @@
-// NOTE: I previously suggested doing this through Grunt, but had plenty of problems with
-// my set up. Grunt did some weird things with scope, and I ended up using nodemon. This
-// setup is now using Gulp. It works exactly how I expect it to and is WAY more concise.
 var gulp = require('gulp'),
     spawn = require('child_process').spawn,
     node;
@@ -31,10 +28,6 @@ gulp.task('default', function() {
   gulp.watch(['./app.js', './routes/**/*.js', './views/**/*.ejs','./utilities.js', './feedmap.js'], function() {
     gulp.run('server')
   })
-
-  // Need to watch for sass changes too? Just add another watch call!
-  // no more messing around with grunt-concurrent or the like. Gulp is
-  // async by default.
 })
 
 // clean up if an error goes unhandled.
